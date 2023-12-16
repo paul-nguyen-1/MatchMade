45442a112a8e539da1a06f7eb852fe614fe38060
paul-nguyen96 <paul121596@icloud.com>
2023-12-16 01:53:46 -0600

    initialize app and set up preloader

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Preloader from './components/Preloader/Preloader'
+
+function App() {
+  return (
+    <>
+    <Preloader/>
+    <h1>Welcome</h1>
+    </>
+  )
+}
+
+export default App
