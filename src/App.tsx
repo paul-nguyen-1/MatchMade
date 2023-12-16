b67467bfc4ca7a41c9f063b12f972e89407b291b
paul-nguyen96 <paul121596@icloud.com>
2023-12-16 02:03:07 -0600

    set up preloader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css'
 import Preloader from './components/Preloader/Preloader'
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fakeDataFetch = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 4000);
+    };
+
+    fakeDataFetch();
+  }, []);
   return (
     <>
-    <Preloader/>
-    <h1>Welcome</h1>
+      {isLoading ? <Preloader /> :
+        <div>
+          <h1>Welcome</h1>
+        </div>}
     </>
   )
 }
