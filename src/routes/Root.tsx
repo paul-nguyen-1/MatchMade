824150fb6da263ec9498d85fa18cb3bc9fd62cbd
paul-nguyen96 <paul121596@icloud.com>
2023-12-16 17:16:12 -0600

    fix build

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -6,7 +6,7 @@ import Footer from '../components/Footer/Footer';
 import { Outlet } from 'react-router-dom';
 
 function Root() {
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const preloadScreen = () => {
--- src/static/ErrorPage.tsx
+++ src/static/ErrorPage.tsx
@@ -1,17 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 export default function ErrorPage() {
-  const error = useRouteError();
-//   console.error(error);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const error = location.state && location.state.error;
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>{error ? error.statusText || error.message : "Unknown error"}</i>
       </p>
-      <a href="/">
+      <a href="/" onClick={() => navigate("/")}>
         <span>Back to home</span>
       </a>
     </div>
