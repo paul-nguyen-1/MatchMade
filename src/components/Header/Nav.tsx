181a21325ef8e07eb5992445fda2f2508ce104b7
paul-nguyen96 <paul121596@icloud.com>
2023-12-17 01:48:53 -0600

    login/signout

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,8 +1,42 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import logo from '../../assets/MatchMadeLogo.webp'
 import './Nav.scss'
+import { useEffect, useState } from 'react';
+import { Session } from '@supabase/supabase-js';
+import supabase from '../../config/supabaseClient';
 
 function Nav() {
+    const navigate = useNavigate();
+    const [session, setSession] = useState<Session | null>(null)
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session)
+        })
+
+        const {
+            data: { subscription },
+        } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session)
+        })
+
+        return () => subscription.unsubscribe()
+    }, [])
+
+    const handleSignOut = async () => {
+        try {
+            const { error } = await supabase.auth.signOut();
+            if (session) {
+                navigate('/');
+            }
+            if (error) {
+                console.error('Sign-out error:', error.message);
+            }
+        } catch (error) {
+            console.error('Error during sign-out:' + error);
+        }
+    };
+
     return (
         <div className='navbar'>
             <Link to="/" className='navLogo'>
@@ -15,7 +49,7 @@ function Nav() {
                 <Link to='/'>Support</Link>
             </div>
             <div className='loginButtons'>
-                <Link to='/account-authentication' className='authentication'>Login / Sign Up</Link>
+                {!session ? <Link to='/account-authentication' className='authentication'>Login / Sign Up</Link> : <Link to='/' className='authentication' onClick={handleSignOut}>Signout</Link>}
             </div>
         </div>
     )
--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,12 +1,38 @@
-import { Auth } from '@supabase/auth-ui-react'
+import { Auth } from '@supabase/auth-ui-react';
 import supabase from '../config/supabaseClient';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
+import { useEffect, useState } from 'react';
+import { Session } from '@supabase/supabase-js';
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-  return (
-    <>
-    {/* TODO: adding 3rd party providers attributes for authentication (providers={['google', 'facebook', 'github']}) */}
-    <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme="dark"/>
-    </>
-  )
+    const navigate = useNavigate();
+    const [session, setSession] = useState<Session | null>(null);
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+            if (session) {
+                navigate('/');
+            }
+        });
+
+        const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+
+            if (session) {
+                navigate('/');
+            }
+        });
+
+        return () => subscription.unsubscribe();
+    }, [navigate]);
+
+    if (!session) {
+        return <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />;
+    } else {
+        return <div>
+            <h1>You're already signed in!</h1>
+        </div>;
+    }
 }
