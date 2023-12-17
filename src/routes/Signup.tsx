60d344dfff6351ad8009f35d60253d66bf463483
paul-nguyen96 <paul121596@icloud.com>
2023-12-17 01:08:10 -0600

    authentication handled by auth import

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,26 +1,8 @@
-import { useState } from 'react'
 import { Auth } from '@supabase/auth-ui-react'
 import supabase from '../config/supabaseClient';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 
 export default function Signup() {
-  const [loading, setLoading] = useState(false)
-  const [email, setEmail] = useState('')
-
-  const handleLogin = async (event : any) => {
-    event.preventDefault()
-
-    setLoading(true)
-    const { error } = await supabase.auth.signInWithOtp({ email })
-
-    if (error) {
-      alert(error.message)
-    } else {
-      alert('Check your email for the login link!')
-    }
-    setLoading(false)
-  }
-
   return (
     <>
     {/* TODO: adding 3rd party providers attributes for authentication (providers={['google', 'facebook', 'github']}) */}
